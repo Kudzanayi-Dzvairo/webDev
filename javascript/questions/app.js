dee8082f4059cf02bcfff02c6b3c2b29cf55de44
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-05-04 20:09:01 -0400

    questions

---
+++ javascript/questions/app.js
@@ -0,0 +1,38 @@
+//using selectors inside the element
+
+const questions = document.querySelectorAll('.question')
+
+questions.forEach(question => {
+    console.log(question)
+    const btn = question.querySelector('.question-btn')
+    console.log(btn)
+    btn.addEventListener('click', () => {
+
+        questions.forEach((item) => {
+          if(item !== question){
+              item.classList.remove('show-text')
+          }
+        })
+
+
+        question.classList.toggle('show-text')
+    })
+})
+
+
+
+
+
+
+
+// traversing the dom
+
+// const btns = document.querySelectorAll('.question-btn')
+
+// btns.forEach(button => {
+//     button.addEventListener('click', e => {
+//         const question = e.currentTarget.parentElement.parentElement
+//         console.log(question)
+//         question.classList.toggle('show-text')
+//     })
+// })
