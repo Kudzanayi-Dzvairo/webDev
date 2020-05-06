5f45d4f21bc7b8fbc476b934f9bd9842fbf733e9
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2020-05-06 07:50:04 -0400

    scroll pj

--- javascript/scroll/app.js
+++ javascript/scroll/app.js
@@ -14,14 +14,16 @@ const  linksContainer = document.querySelector('.links-container')
 const links = document.querySelector('.links')
 
 navToggle.addEventListener('click',() => {
-    linksContainer.classList.toggle('show-links')
-    const containerHeight = linksContainer.getBoundingClientRect()
+    // linksContainer.classList.toggle('show-links')
+
+    const containerHeight = linksContainer.getBoundingClientRect().height
     const linksHeight = links.getBoundingClientRect().height
     if(containerHeight === 0){
-        linksContainer.style.height = `${linksHeight}px`
-    }else{
-        linksContainer.style.height = 0
+     linksContainer.style.height = `${linksHeight}px`;
+    }else {
+    linksContainer.style.height = 0;
     }
+    
 })
 // ********** fixed navbar ************
 const navbar = document.getElementById('nav')
@@ -50,5 +52,26 @@ const scrollLinks = document.querySelectorAll('.scroll-link')
 scrollLinks.forEach(link =>{
     link.addEventListener('click', e => {
         e.preventDefault()
+        //nav to specific spot
+        const id = e.currentTarget.getAttribute('href').slice(1)
+        console.log(id)
+        const element = document.getElementById(id)
+        //calculate the heights
+        const navHeight = navbar.getBoundingClientRect().height
+        const containerHeight = linksContainer.getBoundingClientRect().height
+        const fixedNav =navbar.classList.contains('fixed-nav')
+        let position = element.offsetTop - navHeight;
+
+        if(!fixedNav){
+            position = position -navHeight
+        }
+        if(navHeight > 82){
+            position = position + containerHeight
+        }
+        window.scrollTo({
+            left:0,
+            top:position,
+        })
+        linksContainer.style.height = 0
     })
 })
